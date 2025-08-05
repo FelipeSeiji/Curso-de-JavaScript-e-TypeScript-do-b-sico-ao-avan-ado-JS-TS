19aec01d7ce7d3d0c44af50f249244b36c193dd2
Felipe Seiji <fenishina@gmail.com>
2025-08-05 19:38:18 -0300

    Comentários de código

---
+++ JavaScript/aula4/comentarios.js
@@ -0,0 +1,12 @@
+// E escrever um comentário
+console.log('Olá mundo!'); // Aqui temos outro comentário
+console.log('Linha 1');
+// Comentário
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+//Vai ser ignorado
+
+/**
+ * Um título
+ */
